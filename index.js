2fcba2e20bfa734c3f32839764927f8a16ca9fa9
kelicaleb <kelicaleb7@gmail.com>
2022-08-22 16:02:05 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+const divide = (num1, num2) =>{
+    console.log("number 1 = " + num1);
+    console.log("number 1 = " + num2);
+    return num1 / num2;
+
+}
+let answer = divide(2000, 100)
+console.log(answer)
+const square = (x)=>{
+    console.log("number is = " + x)
+    return x **2;
+}
+let x = square(2);
+console.log(x);
+const add = (num1, num2) =>{
+    console.log("number 1 = " + num1);
+    console.log("number 1 = " + num2);
+    return num1 + num2;
+
+}
+let a = add(3, 4)
+console.log(a)
